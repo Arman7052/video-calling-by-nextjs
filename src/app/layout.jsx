6e5407ecb78358f69434ad65f36e7eaf92b84733
Arman7052 <99arman7052@gmail.com>
2023-11-19 22:41:25 +0600

    Video Call

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script src="AgoraRTC_N-4.19.3.js"></Script>
+      </body>
     </html>
   );
 }
