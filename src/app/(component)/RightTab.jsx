f417526d23e4e208f61adb8a315cad1b4fd97f8d
Arman7052 <99arman7052@gmail.com>
2023-11-19 20:27:04 +0600

    Error fixed

--- src/app/(component)/RightTab.jsx
+++ src/app/(component)/RightTab.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import SideTabsButton from "./(reUsableComponent)/SideTabsButton";
 import AdvicePage from "./(tabComponents)/(rightTab)/Advice";
