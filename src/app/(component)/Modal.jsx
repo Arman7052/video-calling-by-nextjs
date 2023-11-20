dcd8fa98ba7e9a8042ae2b1226b328aa063264ef
Arman7052 <99arman7052@gmail.com>
2023-11-20 20:39:25 +0600

    Modal

--- src/app/(component)/Modal.jsx
+++ src/app/(component)/Modal.jsx
@@ -3,7 +3,7 @@ const Modal = ({ isVisible, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className=" w-full md:w-3/4 flex flex-col ">
+      <div className=" w-full md:w-2/4 flex flex-col ">
         <button
           className="text-white text-sm md:text-xl md:place-self-end "
           onClick={() => onClose()}
